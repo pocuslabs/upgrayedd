58c702887e4ec0668112392862681bd552e80f53
Austin Pocus <ajpocus@gmail.com>
2022-01-03 22:39:13 -0500

    still work to be done, getting registry data

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const axios = require("axios")
 const BASE_NPM_URL = "https://registry.npmjs.org";
 
 const getPackage = async (packageName) => {
-  console.log(await axios.get(`${BASE_NPM_URL}/${packageName}`))
+  const res = await axios.get(`${BASE_NPM_URL}/${packageName}`);
+  return res.data;
 };
 
 const upgrayedd = async () => {
   const fileContents = await fs.readFile("package.json");
   const { dependencies, devDependencies } = JSON.parse(fileContents);
 
-  for (let [packageName, version] of Object.entries(dependencies)) {
-    getPackage(packageName);
+  const outdated = Object.entries(dependencies).filter(async ([packageName, version]) => {
+    const registryData = await getPackage(packageName);
+    const latestVersion = registryData["dist-tags"].latest;
+    if (semver.lt(semver.coerce(version), latestVersion)) {
+      return true;
+    }
+  });
+
+  if (outdated.length) {
+    outdated.forEach(([packageName, version]) => {
+      console.log(`${packageName} (${version}) is out of date!`);
+    });
+  } else {
+    console.log("All up to date! Take a break.")
   }
 };
 
