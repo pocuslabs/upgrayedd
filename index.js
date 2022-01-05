2e22d464647cb5fdbd05de090943574cfef5f43c
Austin Pocus <ajpocus@gmail.com>
2022-01-05 16:15:34 -0500

    fetching deprecations in a rudimentary way

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const semver = require("semver");
 const axios = require("axios");
 
 const BASE_NPM_URL = "https://registry.npmjs.org";
+const GITHUB_BASE_URL = "https://api.github.com";
 
 const parseLockFile = async (lockFileName) => {
   const rawContents = await fs.readFile(lockFileName);
@@ -31,7 +32,9 @@ let upgrayedd = async (packageLockFile) => {
 
     const registryData = await upgrayedd.fetchPackage(packageName);
     const latestVersion = registryData["dist-tags"].latest;
-    const releases = await upgrayedd.fetchReleases(registryData.repository.url);
+    const deprecations = await upgrayedd.fetchReleases(registryData.repository.url, {
+      actualVersion, latestVersion
+    });
 
     packages[packageName] = {
       packageName,
@@ -40,7 +43,8 @@ let upgrayedd = async (packageLockFile) => {
       latestVersion,
       outOfDate: semver.lt(actualVersion, latestVersion),
       satisfied: semver.satisfies(actualVersion, versionSpec),
-      deprecated: false  // TODO: this will be going away once I get the github API integration up
+      deprecated: deprecations.length > 0,  // TODO: this will be going away once I get the github API integration up
+      deprecations
     };
   }
 
@@ -52,13 +56,41 @@ upgrayedd.fetchPackage = async (packageName) => {
   return res.data;
 };
 
-upgrayedd.fetchReleases = async (gitUrl) => {
+upgrayedd.fetchReleases = async (gitUrl, { actualVersion, latestVersion }) => {
   const gitUrlRegex = new RegExp("github\.com/(.+?)/(.+?).git$");
   const matchResult = gitUrl.match(gitUrlRegex);
-  if (!matchResult) { return; }
-  const [totalMatch, owner, repoName] = matchResult;
-  console.log(owner, repoName);
-  return;
+  if (!matchResult) { return []; }
+  const [totalMatch, owner, repo] = matchResult;
+
+  const releaseURL = `${GITHUB_BASE_URL}/repos/${owner}/${repo}/releases`;
+  const releaseRes = await axios.get(releaseURL);
+
+  const releaseData = releaseRes.data;
+  if (!releaseData?.length) {
+    return [];
+  }
+
+  let newerVersions = [];
+  for (let release of releaseData) {
+    if (semver.coerce(release.name) === actualVersion) {
+      break;
+    }
+
+    newerVersions.push(release);
+  }
+
+  newerVersions.reverse();
+  let deprecations = [];
+  for (let release of newerVersions) {
+    const releaseLines = release.body.split("\r\n");
+    for (let line of releaseLines) {
+      if (line.match("deprec")) {
+        deprecations.push(line);
+      }
+    }
+  }
+
+  return deprecations;
 };
 
 upgrayedd.main = () => {
@@ -68,9 +100,15 @@ upgrayedd.main = () => {
 
 if (require.main === module) {
   (async function () {
-    const outdated = await upgrayedd("package-lock.json");
-    for (let [packageName, package] of Object.entries(outdated)) {
+    const deprecations = await upgrayedd("package-lock.json");
+    for (let [packageName, package] of Object.entries(deprecations)) {
       console.log(`${package.packageName} (${package.actualVersion}) is out of date!`);
+      if (package.deprecations?.length) {
+        console.log("Deprecations:");
+        for (let line of package.deprecations) {
+          console.log(line);
+        }
+      }
     }
   })();
 }
